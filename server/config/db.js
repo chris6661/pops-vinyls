31aee5cf49e6e0de45e97e993362429399b75982
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-06-01 13:34:16 -0500

    models, server in server

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser = true,
+            useNewUrlParser: true,
             useUnifiedTopology: true
         }); 
 
@@ -17,4 +17,4 @@ const connectDB = async () => {
     } 
 }
 
-module.export = connectDB;
+module.exports = connectDB;
