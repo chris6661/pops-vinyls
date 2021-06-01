a17b9ecd244dd92b62637908dedb55f3716bc9e4
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-06-01 08:54:13 -0500

    commit

---
+++ server/config/db.js
@@ -0,0 +1,20 @@
+require('dotenv').config(); 
+
+const mongoose = require('mongoose'); 
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser = true,
+            useUnifiedTopology: true
+        }); 
+
+        console.log('MongoDB connection success!')
+
+    } catch(error) {
+        console.error('MongoDB connection not established.')
+        process.exit(1);
+    } 
+}
+
+module.export = connectDB;
